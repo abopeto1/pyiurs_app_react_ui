5540ce4ab5c2bad018f3af9a03662071f22ff909
Arnold Bopeto <abopeto1@gmail.com>
2020-08-11 14:26:17 +0100

    Change customer category tab to bizchart Representation

--- src/components/Layout/Dashboard/CustomerCategory/CustomerCategory.js
+++ src/components/Layout/Dashboard/CustomerCategory/CustomerCategory.js
@@ -1,24 +1,62 @@
 import React from 'react'
-import { Card } from 'antd'
-import { RadialChart } from 'react-vis'
+import { Card, Typography } from 'antd'
+import { Chart, Legend, View, Coord, Geom, Label } from 'bizcharts'
+
 
 export const CustomerCategory = (props) => {
     const { read, entities, status } = props
 
     React.useEffect(read,[])
 
-    const datas = entities ? entities.map(c => ({
-        angle: c.total_customer/ entities.reduce((a,i) => a + i.total_customer, 0),
-        subLabel: c.name,
-
+    const data = entities ? entities.map(c => ({
+        type: c.name,
+        value: c.total_customer,
     })) : []
-
+    
     return (
         <Card title="Client par catégorie" loading={status && status.isFetching}>
-            <RadialChart
-                data={datas} width={300} height={300}
-                animation showLabels
-            />
+            <Typography.Title level={4}>{data && data.reduce((a,i) => a + i.value, 0)}</Typography.Title>
+            <Chart
+            height={500}
+            weight={500}
+            forceFit
+            padding={[20, 0, "auto", 0]}
+          >
+            <Legend />
+            <View
+              data={data}
+              start={{
+                x: 0,
+                y: 0
+              }}
+              end={{
+                x: 1,
+                y: 1
+              }}
+            >
+              <Coord
+                type="theta"
+                startAngle={0}
+                endAngle={Math.PI * 2}
+              />
+              <Geom
+                type="intervalStack"
+                position="value"
+                color="type"
+                shape="rect"
+              >
+                <Label
+                  content={["type*value", (type, value)=>{
+                    return `Client(e)s ${type} : ${value} `
+                  }]}
+                  offset={-20}
+                  textStyle={{
+                    rotate: 0
+                  }}
+                />
+              </Geom>
+            </View>
+          </Chart>
         </Card>
     )
 }
